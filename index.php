2c3f1304ccfc1f2276bd31a07519b6da1f4cc542
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-12-12 08:24:26 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- 
+Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+- L’e-commerce vende **prodotti** per animali.
+- I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.
+- I prodotti saranno oltre al **cibo**, anche **giochi**, **cucce**, etc.
+Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+Allego lo schema fatto in classe (da considerarsi a titolo indicativo ma potete personalizzare come credete)
+ -->
+
+ 
+
+
